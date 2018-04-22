1de62324898c36965e782121ff4086e9eeed0b80
mochiko518 <38201147+mochiko518@users.noreply.github.com>
2018-04-22 22:40:56 +0900

    update

---
+++ 02/homework2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+  int t;
+  double x,x0,xx,k,r;
+  x=1.0;
+  x0=1.0;
+  k=200.0;
+  r=1.0;
+
+  FILE *fp;
+  fp=fopen("output-a.txt", "w");
+
+  fprintf(fp, "%d, %f\n", 0, x0);
+
+  for(t=1; t<300; t++){
+    xx=x+r*(1-x/k)*x;
+
+    fprintf(fp, "%d, %f\n", t, xx);
+    x=xx;
+  }
+
+  fclose(fp);
+
+  return 0;
+}
